d91e1c4df2de8e15034712e8d8a6f311b37609ec
CitiBanco <sasebasultian@gmail.com>
2024-12-05 01:35:08 -0600

    División en varios test

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import data
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,18 +11,19 @@ class UrbanRoutesPage:
     to_field = (By.ID, 'to')
     taxi_button = (By.XPATH, '//button[text()="Pedir un taxi"]')
     comfort_tariff_button = (By.CSS_SELECTOR, 'button[data-for="tariff-card-4"]')
-    phone_button = (By.CSS_SELECTOR, 'div.np-button div.np-text')
+    phone_button = (By.CLASS_NAME, 'np-text')  # Usando Class Name como tipo de selector adicional
     phone_input = (By.ID, 'phone')
     next_button = (By.XPATH, '//button[text()="Siguiente"]')
     confirm_button = (By.XPATH, '//button[text()="Confirmar"]')
-    payment_button = (By.CSS_SELECTOR, 'div.pp-text')
-    add_card_option = (By.CSS_SELECTOR, 'div.pp-title')
+    payment_button = (By.CLASS_NAME, 'pp-text')
+    add_card_option = (By.CLASS_NAME, 'pp-title')
     card_number_input = (By.ID, 'number')
     card_cvv_input = (By.ID, 'code')
     add_card_button = (By.XPATH, '//button[text()="Agregar"]')
     driver_message_input = (By.ID, 'comment')
     blanket_option = (By.XPATH, '//div[contains(@class, "r-sw-label") and text()="Manta y pañuelos"]')
-    ice_cream_plus = (By.CSS_SELECTOR, 'div.counter-plus')
+    ice_cream_plus = (By.CLASS_NAME, 'counter-plus')
+    driver_info_modal = (By.CLASS_NAME, 'modal-driver-info')
 
     def __init__(self, driver):
         self.driver = driver
@@ -34,8 +34,10 @@ class UrbanRoutesPage:
     def set_to(self, to_address):
         self.driver.find_element(*self.to_field).send_keys(to_address)
 
-    def select_comfort_tariff(self):
+    def click_taxi_button(self):
         self.driver.find_element(*self.taxi_button).click()
+
+    def select_comfort_tariff(self):
         self.driver.find_element(*self.comfort_tariff_button).click()
 
     def enter_phone_number(self, phone_number):
@@ -53,7 +55,7 @@ class UrbanRoutesPage:
         self.driver.find_element(*self.card_number_input).send_keys(card_number)
         cvv_field = self.driver.find_element(*self.card_cvv_input)
         cvv_field.send_keys(card_code)
-        ActionChains(self.driver).send_keys(Keys.TAB).perform()
+        self.driver.switch_to.active_element.send_keys(Keys.TAB)  # Simula TAB
         self.driver.find_element(*self.add_card_button).click()
 
     def add_driver_message(self, message):
@@ -65,6 +67,11 @@ class UrbanRoutesPage:
     def request_ice_creams(self, count=2):
         for _ in range(count):
             self.driver.find_element(*self.ice_cream_plus).click()
+
+    def wait_for_driver_info_modal(self, timeout=30):
+        WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_element_located(self.driver_info_modal)
+        )
 
 
 class TestUrbanRoutes:
@@ -78,38 +85,39 @@ class TestUrbanRoutes:
         capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
         cls.driver = webdriver.Chrome(desired_capabilities=capabilities)
 
-    def test_order_taxi(self):
+    def setup_method(self):
         self.driver.get(data.urban_routes_url)
-        routes_page = UrbanRoutesPage(self.driver)
+        self.routes_page = UrbanRoutesPage(self.driver)
 
-        # 1. Configurar la dirección
-        routes_page.set_from(data.address_from)
-        routes_page.set_to(data.address_to)
+    def test_set_route(self):
+        self.routes_page.set_from(data.address_from)
+        self.routes_page.set_to(data.address_to)
+        assert self.routes_page.get_from() == data.address_from
+        assert self.routes_page.get_to() == data.address_to
 
-        # 2. Seleccionar la tarifa Comfort
-        routes_page.select_comfort_tariff()
+    def test_select_plan(self):
+        self.routes_page.click_taxi_button()
+        self.routes_page.select_comfort_tariff()
 
-        # 3. Rellenar el número de teléfono y confirmar código
-        routes_page.enter_phone_number(data.phone_number)
+    def test_fill_phone_number(self):
+        self.routes_page.enter_phone_number(data.phone_number)
         phone_code = retrieve_phone_code(self.driver)
-        routes_page.confirm_phone_code(phone_code)
+        self.routes_page.confirm_phone_code(phone_code)
 
-        # 4. Agregar una tarjeta de crédito
-        routes_page.add_credit_card(data.card_number, data.card_code)
+    def test_fill_card(self):
+        self.routes_page.add_credit_card(data.card_number, data.card_code)
 
-        # 5. Escribir un mensaje para el controlador
-        routes_page.add_driver_message(data.message_for_driver)
+    def test_comment_for_driver(self):
+        self.routes_page.add_driver_message(data.message_for_driver)
 
-        # 6. Pedir manta y pañuelos
-        routes_page.request_blanket_and_tissues()
+    def test_order_blanket_and_handkerchiefs(self):
+        self.routes_page.request_blanket_and_tissues()
 
-        # 7. Pedir 2 helados
-        routes_page.request_ice_creams()
+    def test_order_2_ice_creams(self):
+        self.routes_page.request_ice_creams()
 
-        # 8. Esperar al modal
-        WebDriverWait(self.driver, 30).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'div.modal-driver-info'))
-        )
+    def test_car_search_model_appears(self):
+        self.routes_page.wait_for_driver_info_modal()
 
     @classmethod
     def teardown_class(cls):
